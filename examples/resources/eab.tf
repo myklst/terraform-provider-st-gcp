2d922edefa6dd4b55a15314dd2a8794d770c9e97
Fuchun Zhang <ahfuzhang@gmail.com>
2023-08-11 10:01:03 +0800

    remove credentials json from resource, use provider's data

--- examples/resources/eab.tf
+++ examples/resources/eab.tf
@@ -16,16 +16,16 @@ provider "st-gcp" {
   credentials = file("projectid-6f5d9ed9a85d.json")
 }
 
-resource "st-gcp_eab" "eab" {
-  credentials_json = file("projectid-6f5d9ed9a85d.json")
+resource "st-gcp_acme_eab" "eab" {
+  eab_account_expires_days = 60
 }
 
 output "eab_key_id" {
   description = "The eab account of key id"
-  value       = st-gcp_eab.eab.key_id
+  value       = st-gcp_acme_eab.eab.key_id
 }
 
 output "eab_hmac_base64" {
   description = "The eab account of hmac_base64"
-  value       = st-gcp_eab.eab.hmac_base64
+  value       = st-gcp_acme_eab.eab.hmac_base64
 }

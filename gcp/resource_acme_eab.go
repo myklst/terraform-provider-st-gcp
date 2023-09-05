06d859ef5eb6a9fbbe4c7e92d3a6e4733746649a
Fuchun Zhang <ahfuzhang@gmail.com>
2023-09-05 12:04:38 +0800

    1.Read:remove code because no need to read state; 2.OpenAPI: add retry when net errors

--- gcp/resource_acme_eab.go
+++ gcp/resource_acme_eab.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
@@ -94,25 +95,8 @@ func (r *acmeEabResource) Create(ctx context.Context, req resource.CreateRequest
 }
 
 func (r *acmeEabResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	var state acmeEabState
-	d := req.State.Get(ctx, &state)
-	resp.Diagnostics.Append(d...)
-	if resp.Diagnostics.HasError() {
-		tflog.Error(ctx, "Read req.State.Get error")
-		return
-	}
-
-	eabData := externalAccountKeyResp{
-		Name:      state.Name.String(),
-		KeyId:     state.KeyID.String(),
-		B64MacKey: state.HmacBase64.String(),
-	}
-	if len(eabData.Name) == 0 || len(eabData.KeyId) == 0 || len(eabData.B64MacKey) == 0 {
-		tflog.Error(ctx, "credential has not been created")
-		resp.Diagnostics.AddError("credential data empty", "credential has not been created")
-		return
-	}
-	resp.State.Set(ctx, &state)
+	// gcp acme eab api not support get open api
+	// and tf framework will auto read state
 }
 
 func (r *acmeEabResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
@@ -139,6 +123,11 @@ func (r *acmeEabResource) Update(ctx context.Context, req resource.UpdateRequest
 func (r *acmeEabResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
 	// Since GCP does not provide an API to delete EAB credential, the delete function will not be implemented.
 }
+
+const (
+	maxRetryTimes = 3
+	retrySleepMs  = 500
+)
 
 // createEabCred Create a EAB credential.
 // see: https://cloud.google.com/certificate-manager/docs/reference/public-ca/rest/v1/projects.locations.externalAccountKeys/create
@@ -167,15 +156,37 @@ func createEabCred(ctx context.Context, s *acmeEabState, credentialsJSON []byte,
 
 	var api = fmt.Sprintf("https://publicca.googleapis.com/v1beta1/projects/%s/locations/global/externalAccountKeys", cred.ProjectId)
 	var resp *http.Response
+	var postData *bytes.Reader
 	if old != nil {
 		old.B64MacKey = base64.StdEncoding.Strict().EncodeToString([]byte(old.B64MacKey))
 		buf, _ := json.Marshal(old)
-		resp, err = conf.Client(context.Background()).Post(api, "application/json", bytes.NewReader(buf))
-	} else {
-		resp, err = conf.Client(context.Background()).Post(api, "application/json", nil)
+		postData = bytes.NewReader(buf)
+	}
+	sleepMs := retrySleepMs
+	retry := 0
+	for ; retry < maxRetryTimes; retry++ {
+		if old != nil {
+			resp, err = conf.Client(context.Background()).Post(api, "application/json", postData)
+		} else {
+			resp, err = conf.Client(context.Background()).Post(api, "application/json", nil)
+		}
+		if err != nil {
+			tflog.Warn(ctx, "post data error:"+err.Error())
+			errMsg := err.Error()
+			if strings.Contains(errMsg, "timeout") ||
+				strings.Contains(errMsg, " 500 ") ||
+				strings.Contains(errMsg, " 504 ") ||
+				strings.Contains(errMsg, "DNS") {
+				time.Sleep(time.Duration(sleepMs) * time.Millisecond)
+				sleepMs *= 2
+				continue
+			}
+			break
+		}
+		break
 	}
 	if err != nil {
-		return fmt.Errorf("failed to request Google Public CA API: %v", err)
+		return err
 	}
 	defer resp.Body.Close()
 
